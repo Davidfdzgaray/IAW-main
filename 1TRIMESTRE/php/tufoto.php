fa8b523037809b315c29fcd7268c386f02d2aacc
David Fernández <davidfernandez02@iesamachado.org>
2023-12-04 14:21:30 +0100

    df

--- 1TRIMESTRE/php/tufoto.php
+++ 1TRIMESTRE/php/tufoto.php
@@ -6,7 +6,7 @@
     <title>TU FOTO</title>
 </head>
 <body>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
         Nombre: <input type="text" name="nombre"><br>
         Foto: <input type="image" src="" alt="" name="foto"><br>
         <input type="submit" value="Enviar" name='submit'>
