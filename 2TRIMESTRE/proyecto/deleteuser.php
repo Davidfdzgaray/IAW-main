8f7481414695ec9a3033d19a2ba94fa7a3d462d4
davidfdzgaray <davidfernandez04@iesamachado.org>
2024-01-22 20:32:13 +0100

    cbg

--- 2TRIMESTRE/proyecto/deleteuser.php
+++ 2TRIMESTRE/proyecto/deleteuser.php
@@ -2,8 +2,12 @@
 <?php 
     if(isset($_GET['eliminarusuario'])) {
         $id= htmlspecialchars($_GET['eliminarusuario']);
-        $query = "DELETE FROM usuarios WHERE id = {$id}"; 
+        $query = "DELETE FROM usuarios WHERE id = {$id};/*DELETE FROM incidencias WHERE id_usuario = {$id};*/"; 
         $delete_query= mysqli_query($conn, $query);
+
+        //$queryr = "DELETE FROM incidencias WHERE id_usuario = {$id}"; 
+        //$delete_queryr= mysqli_query($conn, $queryr);
+
         echo "<script>window.location='users.php';</script>";
     }
 ?>
