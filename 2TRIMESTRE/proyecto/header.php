64a0606762a24df98aa12387dc99e7a0aeb9f6dd
david fernandez <davidfernandez02@iesamachado.org>
2024-03-05 11:57:03 +0100

    vcb

--- 2TRIMESTRE/proyecto/header.php
+++ 2TRIMESTRE/proyecto/header.php
@@ -10,6 +10,12 @@
 <!-- Bootstrap Icon -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Gestión de incidencias (CRUD)</title>
+    <style>
+        .table th a {
+            color: white;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
     
