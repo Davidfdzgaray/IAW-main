9b41b5cb2e22306f4e448db2646a777c480518a8
davidfdzgaray <davidfernandez02@iesamachado.org>
2024-03-09 19:49:25 +0100

    e

--- 2TRIMESTRE/proyecto/footer.php
+++ 2TRIMESTRE/proyecto/footer.php
@@ -1,6 +1,19 @@
 <footer class="blockquote-footer fixed-bottom">Gestión de incidencias del <a href="https://iesamachado.org" target="_blank">IES Antonio Machado</a>. Desarrollado por David Fernández Garay<br>
 <span class="navbar-text" style="color: black;">
-    <img src="./media/3289576_user_icon.png" width="20" height="20" alt=""> <?php echo $_SESSION["usuario"];?><br>
+    <?php
+        $username = $_SESSION["usuario"];
+        $query = "SELECT * FROM usuarios WHERE username = '$username'";
+        $vista_usuario= mysqli_query($conn,$query);
+
+        while($row = mysqli_fetch_assoc($vista_usuario)) {
+            if (empty($row['imagen'])) {
+                $imagen='./media/3289576_user_icon.png';
+            } else {
+                $imagen=$row['imagen'];
+            }
+        } 
+    ?>
+    <img src="<?php echo $imagen; ?>" width="20" height="20" alt=""> <?php echo $_SESSION["usuario"];?><br>
     <?php echo "Ultima Conexión: " . $date . " " . $time;?><br>
     <?php echo "Dirección de Ultima Conexión: " . $ip;?>
 </span>
